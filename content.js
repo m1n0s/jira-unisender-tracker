8d2cc02fa9040b026d371ff482da4accee9489bf
m1n0s <leontiy1992@gmail.com>
2016-11-22 20:02:37 +0200

    WIP add/remove w/o reload

--- content.js
+++ content.js
@@ -1,8 +1,25 @@
 'use strict';
 
-//const user = 'lhrabovetskyi';
+const USER = 'lhrabovetskyi';
 const storeKey = 'JIRAUniSenderTracker';
 
+const ADD_TEXT = 'Add this task for report';
+const REMOVE_TEXT = 'Remove this task from report';
+const ADD_CLASS = 'add';
+const REMOVE_CLASS = 'remove';
+
+const BTN_ID = `${USER}-tasks-toggle`;
+
+const viewIssueSidebar = $('viewissuesidebar');
+const peopleModule = $('peoplemodule');
+
+const CURRENT_TASK = getCurrentTask();
+const CURRENT_TASK_KEY = Object.keys(CURRENT_TASK)[0];
+
+getDataFromStorage(storeKey, data => {
+  renderButton(data);
+});
+/*
 chrome.storage.sync.get(storeKey, data => {
 
   let tasks = data[storeKey] || {};
@@ -16,7 +33,7 @@ chrome.storage.sync.get(storeKey, data => {
   const peopleModule = document.getElementById('peoplemodule');
 
   let btn = document.createElement('button');
-  let caption = document.createTextNode(`${alreadyAdded ? 'Remove this task from report' : 'Add this task for report'}`);
+  let caption = document.createTextNode(`${alreadyAdded ? REMOVE_TEXT : ADD_TEXT}`);
 
   btn.appendChild(caption);
   btn.classList.add('report-btn', `${alreadyAdded ? 'remove' : 'add'}`);
@@ -32,13 +49,12 @@ chrome.storage.sync.get(storeKey, data => {
     }
 
     chrome.storage.sync.set({[storeKey]: tasks}, () => {
-      //alert('Task status saved');
       location.reload();//TODO make this possible without reload
     });
 
   }, false);
 
-});
+});*/
 
 function getCurrentTask() {
   let main = document.querySelector('.aui-page-header-main');
@@ -48,4 +64,72 @@ function getCurrentTask() {
   return {
     [code]: name
   }
+}
+
+function getDataFromStorage(storeKey, callback) {
+  chrome.storage.sync.get(storeKey, data => {
+    callback(data);
+  });
+}
+
+function renderButton(data) {
+
+  let tasks = data[storeKey] || {};
+
+  console.log(tasks);
+   //We always know that this obj has only one key/value
+
+  let alreadyAdded = Object.keys(tasks).some(key => {
+    /*console.log(key);
+     console.log(currentTaskKey);
+     console.log(key === currentTaskKey);*/
+    return key === CURRENT_TASK_KEY
+  });
+
+  let btn = $(BTN_ID);
+
+  if (!btn) {
+    btn = document.createElement('button');
+    btn.id = BTN_ID;
+
+    let caption = document.createTextNode(`${alreadyAdded ? REMOVE_TEXT : ADD_TEXT}`);
+
+    btn.appendChild(caption);
+    btn.classList.add('report-btn', `${alreadyAdded ? REMOVE_CLASS : ADD_CLASS}`);
+
+    viewIssueSidebar.insertBefore(btn, peopleModule.nextSibling);
+
+    btn.addEventListener('click', () => {
+
+      let updatedTasks;
+
+      if (alreadyAdded) {
+        console.log(tasks);
+        let filteredTasks = Object.keys(tasks).map(task => task !== CURRENT_TASK);
+        console.log(filteredTasks);
+        updatedTasks = Object.assign({}, filteredTasks);
+      } else {
+        updatedTasks = Object.assign({}, tasks, CURRENT_TASK);
+      }
+
+      chrome.storage.sync.set({[storeKey]: updatedTasks}, () => {
+        getDataFromStorage(storeKey, data => {
+          renderButton(data);
+        });
+      });
+
+    }, false);
+
+  } else {
+
+    btn.classList.remove(ADD_CLASS, REMOVE_CLASS);
+    btn.classList.add(`${alreadyAdded ? REMOVE_CLASS : ADD_CLASS}`);
+    btn.textContent = `${alreadyAdded ? REMOVE_TEXT : ADD_TEXT}`;
+
+  }
+
+}
+
+function $(id) {
+  return document.getElementById(id);
 }
--- popup.js
+++ popup.js
@@ -1,7 +1,48 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const tasksContainer = document.getElementById('tasks');
-  //const user = 'lhrabovetskyi';
+
+  const tasksContainer = $('tasks');
+  const copyResult = $('copy-result');
+  const copyBtn = $('copy-to-clipboard');
+  //const userKey = 'JIRAUniSenderTrackerUser';
   const storeKey = 'JIRAUniSenderTracker';
+  //const user = getUserFromStorage(userKey);
+
+  addTasksFromStoreToContainer(tasksContainer, storeKey);
+
+  copyBtn.addEventListener('click', (event) => {
+
+    event.preventDefault();
+    let range = document.createRange();
+    range.selectNodeContents(tasksContainer);
+    window.getSelection().addRange(range);
+
+    let success = document.execCommand('copy');
+    copyResult.textContent = `${success ? 'Copied!' : 'Error.'}`;
+
+    setTimeout(() => {
+      copyResult.textContent = '';
+    }, 1500);
+
+    window.getSelection().removeAllRanges();
+
+  }, false);
+
+
+});
+
+
+
+
+function getUserFromStorage(key) {
+
+  chrome.storage.sync.get(key, data => {
+    return data[key]
+  });
+
+}
+
+
+function addTasksFromStoreToContainer(container, storeKey) {
 
   chrome.storage.sync.get(storeKey, data => {
 
@@ -12,18 +53,21 @@ document.addEventListener('DOMContentLoaded', () => {
       output = Object
         .keys(tasks)
         .map(key => `${key} - ${tasks[key]}`)
-        .join('\n\r');
+        .join('\n');
     } else {
       output = `You haven't tracked tasks yet`;
     }
 
     let text = document.createTextNode(output);
+    container.appendChild(text);
 
-    tasksContainer.appendChild(text);
-
+    return text
   });
+}
 
-});
+function $(id) {
+  return document.getElementById(id);
+}
 
 
 
